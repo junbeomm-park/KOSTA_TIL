1a83b1498c61a527d9f60572cb75bdaf4a56ff05
JUNBEOM PARK <junbum0674@gmail.com>
2022-01-24 16:18:59 +0900

    KOSTA_TIL

--- workspace/Java_work/Java236/src/kosta/basic/Member.java
+++ workspace/Java_work/Java236/src/kosta/basic/Member.java
@@ -1,5 +1,16 @@
 package kosta.basic;
 
+/*
+ * 제목 : 회원관리
+ * 날짜 : 2022년 1월 24일
+ * 작성자 : 홍길동  
+ */
 public class Member {
-
+	private String name;	//이름 
+	private int age;	//나이 
+	private String phoneNo;	//전화번호 
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 }
