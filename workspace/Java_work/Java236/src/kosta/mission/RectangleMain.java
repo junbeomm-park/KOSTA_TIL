c5f87eef80055a6d1e09ab64460387c9f5a96178
junbeomm-park <88473491+junbeomm-park@users.noreply.github.com>
2022-02-10 15:47:46 +0900

    KOSTA_TIL
    접근 제어자

---
+++ workspace/Java_work/Java236/src/kosta/mission/RectangleMain.java
@@ -0,0 +1,16 @@
+package kosta.mission;
+
+import kosta.oop.Rectangle;
+
+public class RectangleMain {
+
+	public static void main(String[] args) {
+		Rectangle r = new Rectangle();
+		
+		r.setWidth(10);
+		r.setHeight(5);
+		r.getArea();
+		
+	}
+
+}
