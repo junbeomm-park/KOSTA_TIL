0a50dd9084222e238d2eaea59de02d262cf3c18c
junbeomm-park <88473491+junbeomm-park@users.noreply.github.com>
2022-02-13 21:08:51 +0900

    Update Solution2_11.java

--- workspace/Java_work/Java236/src/kosta/mission2/Solution2_11.java
+++ workspace/Java_work/Java236/src/kosta/mission2/Solution2_11.java
@@ -6,25 +6,40 @@ public class Solution2_11 {
 	
 	public void solution(int n, int arr[][]) {
 		int answer = 0;
+		int max = 0;
+		int count= 0;
 		
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < n; j++) {
-				
+				for(int k = 0; k < 5; k++) {
+					if(arr[i][j] == arr[j][k] && i != j) {
+						count ++;
+						break;
+					}
+				}
+			}
+			if(count > max) {
+				answer = i;
+				max = count;
 			}
 		}
+		System.out.println(answer);
 	}
 
 	public static void main(String[] args) {
+		
 		Solution2_11 s = new Solution2_11();
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int[][] arr = new int[n][n];
+		int[][] arr = new int[n][5];
 		
 		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
+			for(int j = 0; j < 5; j++) {
 				arr[i][j] = sc.nextInt();
 			}
 		}
+		
+		s.solution(n, arr);
 	}
 
 }
