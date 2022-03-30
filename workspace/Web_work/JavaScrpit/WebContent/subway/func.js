26af4de6f13cb686ed7418593cc8fd53a20e52e3
junbeomm-park <88473491+junbeomm-park@users.noreply.github.com>
2022-03-30 17:47:37 +0900

    KOSTA_TIL
    dom, ajax

---
+++ workspace/Web_work/JavaScrpit/WebContent/subway/func.js
@@ -0,0 +1,21 @@
+function calculte(){
+	var adult = 0;
+	var student = 0;
+	var chid = 0;
+	
+    const basicFee = function(){
+    var start = document.getElementById("start");
+	var end = document.getElementById("end");
+	var count = 0;
+	   for(var i = 0; i < subwayList.length; i++){
+	    	 start = start.subwayList[i];
+	         end = end.subwayList[i];
+	    		  
+	         count = end - start;
+	    		  
+	         console.log(count);
+	    		  
+	    		  
+	    	  }
+      }
+}
